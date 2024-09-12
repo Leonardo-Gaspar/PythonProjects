dfe254a2b543a4ec1e7e8c50b08045e3bc6e6e19
Leonardo Gaspar Saheb <leosaheb2003@gmail.com>
2024-09-12 15:05:43 -0300

    Update LangchainAgent

---
+++ LangchainAgents/main.py
@@ -0,0 +1,29 @@
+'''
+LangSmith: Repositório com vários tipos de prompts e conversações
+LangSmith -> openai-functions-agent
+
+Agent: 
+       -> llm         -> llm
+       -> Ferramentas -> tools
+       -> prompt      -> prompt
+'''
+from langchain.agents import AgentExecutor
+from agent import AgenteOpenAIFunctions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pergunta = "Quais os dados de Ana?"
+pergunta = "Quais os dados de Bianca?"
+pergunta = "Quais os dados de Ana e da Bianca?"
+
+agente = AgenteOpenAIFunctions()
+executor = AgentExecutor(agent=agente.agente,
+                        tools=agente.tools,
+                        verbose=True)
+resposta = executor.invoke({"input" : pergunta})
+print(resposta)
+
+
+
+
